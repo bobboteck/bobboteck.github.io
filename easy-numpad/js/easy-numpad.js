26726d17e94f05798c9d0f0d778b20d29aa9e8f3
bobboteck <bobboteck@gmail.com>
2019-04-30 18:22:42 +0200

    Update of easy-numpad demo

--- easy-numpad/js/easy-numpad.js
+++ easy-numpad/js/easy-numpad.js
@@ -13,26 +13,26 @@ function show_easy_numpad(thisElement)
         <div class="easy-numpad-number-container">
             <table>
                 <tr>
-                    <td><a href="7" onclick="easynum()">7</a></td>
-                    <td><a href="8" onclick="easynum()">8</a></td>
-                    <td><a href="9" onclick="easynum()">9</a></td>
+                    <td><a href="7" onclick="easynum(this)">7</a></td>
+                    <td><a href="8" onclick="easynum(this)">8</a></td>
+                    <td><a href="9" onclick="easynum(this)">9</a></td>
                     <td><a href="Del" class="del" id="del" onclick="easy_numpad_del()">Del</a></td>
                 </tr>
                 <tr>
-                    <td><a href="4" onclick="easynum()">4</a></td>
-                    <td><a href="5" onclick="easynum()">5</a></td>
-                    <td><a href="6" onclick="easynum()">6</a></td>
+                    <td><a href="4" onclick="easynum(this)">4</a></td>
+                    <td><a href="5" onclick="easynum(this)">5</a></td>
+                    <td><a href="6" onclick="easynum(this)">6</a></td>
                     <td><a href="Clear" class="clear" id="clear" onclick="easy_numpad_clear()">Clear</a></td>
                 </tr>
                 <tr>
-                    <td><a href="1" onclick="easynum()">1</a></td>
-                    <td><a href="2" onclick="easynum()">2</a></td>
-                    <td><a href="3" onclick="easynum()">3</a></td>
+                    <td><a href="1" onclick="easynum(this)">1</a></td>
+                    <td><a href="2" onclick="easynum(this)">2</a></td>
+                    <td><a href="3" onclick="easynum(this)">3</a></td>
                     <td><a href="Cancel" class="cancel" id="cancel" onclick="easy_numpad_cancel()">Cancel</a></td>
                 </tr>
                 <tr>
-                    <td colspan="2" onclick="easynum()"><a href="0">0</a></td>
-                    <td onclick="easynum()"><a href=".">.</a></td>
+                    <td colspan="2" onclick="easynum(this)"><a href="0">0</a></td>
+                    <td onclick="easynum(this)"><a href=".">.</a></td>
                     <td><a href="Done" class="done" id="done" onclick="easy_numpad_done()">Done</a></td>
                 </tr>
             </table>
@@ -51,30 +51,36 @@ function easy_numpad_close()
     elementToRemove.parentNode.removeChild(elementToRemove);
 }
 
-function easynum() {
+function easynum(thisElement)
+{
     event.preventDefault();
+    document.getElementById("easy-numpad-output").innerText += thisElement.innerText;
+}
 
-    var easy_num_button = $(event.target);
-    var easy_num_value = easy_num_button.text();
-    $('#easy-numpad-output').append(easy_num_value);
-}
-function easy_numpad_del() {
+function easy_numpad_del()
+{
     event.preventDefault();
-    var easy_numpad_output_val = $('#easy-numpad-output').text();
+    let easy_numpad_output_val = document.getElementById("easy-numpad-output").innerText;
     var easy_numpad_output_val_deleted = easy_numpad_output_val.slice(0, -1);
-    $('#easy-numpad-output').text(easy_numpad_output_val_deleted);
+    document.getElementById("easy-numpad-output").innerText = easy_numpad_output_val_deleted;
 }
-function easy_numpad_clear() {
+
+function easy_numpad_clear()
+{
     event.preventDefault();
-    $('#easy-numpad-output').text("");
+    document.getElementById("easy-numpad-output").innerText="";
 }
-function easy_numpad_cancel() {
+
+function easy_numpad_cancel()
+{
     event.preventDefault();
-    $('#easy-numpad-frame').remove();
+    easy_numpad_close();
 }
-function easy_numpad_done() {
+
+function easy_numpad_done() 
+{
     event.preventDefault();
-    var easy_numpad_output_val = $('#easy-numpad-output').text();
+    let easy_numpad_output_val = document.getElementById("easy-numpad-output").innerText;
     document.getElementById(_outputID).value=easy_numpad_output_val;
     easy_numpad_close();
 }
