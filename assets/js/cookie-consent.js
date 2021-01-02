e7d41da3ef7cfbffb4904530b487c492757d3a50
bobboteck <bobboteck@gmail.com>
2021-01-02 16:36:29 +0100

    changed position of js code

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,14 @@
+window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#000"
+      },
+      "button": {
+        "background": "#f1d600"
+      }
+    },
+    "type": "opt-in",
+    "content": {
+      "href": "https://bobboteck.github.io/terms/"
+    }
+  });
