1655db8d045c52a10c4a93a7f76cf6f1130be030
bobboteck <bobboteck@gmail.com>
2021-01-02 16:56:14 +0100

    add moro control on cookies

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -1,14 +1,61 @@
-window.cookieconsent.initialise({
-    "palette": {
-      "popup": {
-        "background": "#000"
-      },
-      "button": {
-        "background": "#f1d600"
-      }
+window.cookieconsent.initialise(
+{
+    "palette": 
+    {
+        "popup": 
+        {
+            "background": "#000"
+        },
+        "button": 
+        {
+            "background": "#f1d600"
+        }
     },
     "type": "opt-in",
-    "content": {
-      "href": "https://bobboteck.github.io/terms/"
+    "content": 
+    {
+        "message": "This website uses cookies to ensure you get the best experience here.",
+        "href": "https://bobboteck.github.io/terms/"
+    },
+    onInitialise: function (status) 
+    {
+        var type = this.options.type;
+        var didConsent = this.hasConsented();
+        if (type == 'opt-in' && didConsent)
+        {
+            // enable cookies
+            loadGAonConsent();
+        }
+        if (type == 'opt-out' && !didConsent)
+        {
+            // disable cookies
+        }
+    },
+    onStatusChange: function(status, chosenBefore)
+    {
+        var type = this.options.type;
+        var didConsent = this.hasConsented();
+        if (type == 'opt-in' && didConsent)
+        {
+            // enable cookies
+            loadGAonConsent();
+        }
+        if (type == 'opt-out' && !didConsent)
+        {
+            // disable cookies
+        }
+    },
+    onRevokeChoice: function()
+    {
+        var type = this.options.type;
+        if (type == 'opt-in')
+        {
+            // disable cookies
+        }
+        if (type == 'opt-out')
+        {
+            // enable cookies
+            loadGAonConsent();
+        }
     }
-  });
+});
