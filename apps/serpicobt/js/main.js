ece6a73e02ca568b1715a2fc5c6882e2c42ed53e
bobboteck <bobboteck@gmail.com>
2021-03-23 12:50:25 +0100

    Test BT

---
+++ apps/serpicobt/js/main.js
@@ -0,0 +1,17 @@
+const connectButton = document.getElementById('connect');
+const disconnectButton = document.getElementById('disconnect');
+
+
+
+
+connectButton.addEventListener('click', () => 
+{
+    navigator.bluetooth.requestDevice({filters: [{Services: 0xFFE0}]})
+    .then(device => { console.log(device) })
+    .catch(error => { console.log(error); });
+});
+  
+disconnectButton.addEventListener('click', () => 
+{
+
+});
