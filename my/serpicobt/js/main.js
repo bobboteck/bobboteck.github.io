f87dd7d9b61b675f48d3738848f12154369c1e7f
bobboteck <bobboteck@gmail.com>
2021-03-23 14:26:07 +0100

    ok

--- my/serpicobt/js/main.js
+++ my/serpicobt/js/main.js
@@ -2,11 +2,12 @@ const connectButton = document.getElementById('connect');
 const disconnectButton = document.getElementById('disconnect');
 
 
+let serviceUuid = parseInt("0xFFE0");
 
 
 connectButton.addEventListener('click', () => 
 {
-    navigator.bluetooth.requestDevice({ filters: [ { Services: [ 0xFFE0 ] } ] })
+    navigator.bluetooth.requestDevice({ filters: [ { Services: [ serviceUuid ] } ] })
     .then(device => { console.log(device) })
     .catch(error => { console.log(error); });
 });
