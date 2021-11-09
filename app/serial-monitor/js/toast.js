ba8f3738b279a1693fa7d8f5d696f6aa046ae3e8
bobboteck <bobboteck@gmail.com>
2021-11-09 19:08:48 +0100

    Test

--- app/serial-monitor/js/toast.js
+++ app/serial-monitor/js/toast.js
@@ -2,7 +2,11 @@ let noSupportToastTime = document.getElementById("noSupportToastTime");
 let noSupportToast = document.getElementById("noSupportToast");
 let noSupportToastClose = document.getElementById("noSupportToastClose");
 
-if(!"serial" in navigator) 
+if("serial" in navigator)
+{
+
+}
+else
 {
     let currentdate = new Date(); 
     let datetime = currentdate.getDate() + "/" + (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear() + " " + currentdate.getHours() + ":" + currentdate.getMinutes();
